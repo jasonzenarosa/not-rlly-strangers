faf95315e5d6f385acddd2fb964fd91dbfd70433
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-11-04 15:44:09 -0700

    Basic site functionality

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 import { levelOne, levelTwo, levelThree } from "./data.js";
 
+let card = document.querySelector('.card');
+let button = document.querySelector('.draw')
+
 function drawCard(deck) {
     let randomCardIndex = Math.floor(Math.random() * deck.length);
     return deck[randomCardIndex];
 }
 
-console.log(drawCard(levelOne));
-// console.log(drawCard(levelTwo));
-// console.log(drawCard(levelThree));
+function changeCardText(text) {
+    card.textContent = text;
+}
+
+changeCardText(drawCard(levelOne))
+button.addEventListener("click", _ => changeCardText(drawCard(levelOne)))
