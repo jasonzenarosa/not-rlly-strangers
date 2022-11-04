033a69a2b6f99ccb32ca2080d77c7609d8bcdcf9
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-11-04 15:31:53 -0700

    Created drawCard fn, added other required files

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-import { levelOne, levelTwo, levelThree } from "./data";
+import { levelOne, levelTwo, levelThree } from "./data.js";
+
+function drawCard(deck) {
+    let randomCardIndex = Math.floor(Math.random() * deck.length);
+    return deck[randomCardIndex];
+}
+
+console.log(drawCard(levelOne));
+// console.log(drawCard(levelTwo));
+// console.log(drawCard(levelThree));
