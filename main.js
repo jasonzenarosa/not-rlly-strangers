14dca059d5ed2c9a98574cc241a28a589db523be
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-11-05 05:02:09 -0700

    Deck switching

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import { levelOne, levelTwo, levelThree } from "./data.js";
 
 let card = document.querySelector('.card');
-let button = document.querySelector('.draw')
+let button = document.querySelector('.draw');
+let options = document.getElementById('deck')
+let cur_deck = levelOne;
 
 function drawCard(deck) {
     let randomCardIndex = Math.floor(Math.random() * deck.length);
@@ -12,5 +14,17 @@ function changeCardText(text) {
     card.textContent = text.toUpperCase();
 }
 
-changeCardText(drawCard(levelOne))
-button.addEventListener("click", _ => changeCardText(drawCard(levelOne)));
+function setCurDeck(event) {
+    console.log(event.target.value)
+    if (event.target.value == 'l1') {
+        cur_deck = levelOne;
+    } else if (event.target.value == 'l2') {
+        cur_deck = levelTwo
+    } else if (event.target.value == 'l3') {
+        cur_deck = levelThree
+    }
+}
+
+changeCardText(drawCard(cur_deck))
+button.addEventListener("click", _ => changeCardText(drawCard(cur_deck)));
+options.addEventListener("change", setCurDeck)
