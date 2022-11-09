7714ba1580faeee4ed93ce4c7c28e19de67d7a86
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-11-08 18:21:59 -0800

    Refactored to use NRSAPI

--- main.js
+++ main.js
@@ -1,43 +1,19 @@
-import { levelOne, levelTwo, levelThree } from "./data.js";
-
 let card = document.querySelector('.card');
 let button = document.querySelector('.draw');
 let options = document.getElementById('deck')
-let cur_deck = levelOne;
+let cur_deck = 'level1';
 
-function drawCard(deck) {
-    let randomCardIndex = Math.floor(Math.random() * deck.length);
-    return deck[randomCardIndex];
-}
 
-function changeCardText() {
-    clearInterval(timer)
-    var op = 0;
-    card.style.opacity = 0
-    card.textContent = drawCard(cur_deck).toUpperCase();
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-            return
-        }
-        card.style.opacity = op;
-        op *= 100;
-        op += 1;
-        op /= 100;
-    }, 10);
+function drawCard() {
+    fetch(`https://not-rlly-strangers-api.herokuapp.com//${cur_deck}`)
+    .then(response => response.json())
+    .then(json => card.textContent = json.card.toUpperCase())
 }
 
 function setCurDeck(event) {
-    console.log(event.target.value);
-    if (event.target.value == 'l1') {
-        cur_deck = levelOne;
-    } else if (event.target.value == 'l2') {
-        cur_deck = levelTwo;
-    } else if (event.target.value == 'l3') {
-        cur_deck = levelThree;
-    }
+    cur_deck = event.target.value
 }
 
-changeCardText(drawCard(cur_deck));
-button.addEventListener("click", changeCardText);
+drawCard()
+button.addEventListener("click", drawCard);
 options.addEventListener("change", setCurDeck);
